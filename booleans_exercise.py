f7749396c8da17fa61752f93d4f7f277a85aa847
NicLambie <nic_lambie@hotmail.com>
2020-07-25 16:55:36 +1000

    Exercises for SheCodes

---
+++ booleans_exercise.py
@@ -0,0 +1,9 @@
+# question 1
+
+moths_in_the_house = False
+
+if moths_in_the_house:
+    print("Get the moths, Roary!")
+else:
+    print("No threat detected, Rorary.")
+
---
+++ lists_exercise.py
@@ -0,0 +1,42 @@
+# question1
+
+foods = [
+"orange",
+"apple",
+"banana",
+"strawberry",
+"grape",
+"blueberry",
+["carrot", "cauliflower", "pumpkin"],
+"passionfruit",
+"mango",
+"kiwifruit"
+]
+
+print(foods)
+
+print(foods[0])
+print(foods[2])
+print(foods[9])
+
+print(foods[7:10])
+
+# question2
+mailing_list = [
+["Roary", "roary@moth.catchers"],
+["Remus", "remus@kapers.dog"],
+["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+["Biscuit", "biscuit@whippies.park"],
+["Rory", "rory@whippies.park"],
+]
+
+for item in (mailing_list):
+    print(f"{item[0]}: {item[1]}")
+
+# question3
+names_list = [
+    [Izzy]
+    [Archie]
+    [Boston]
+]
+
